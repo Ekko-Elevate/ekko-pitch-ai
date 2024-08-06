504ac1a5593a5ea3a3502cff1bfc4251ef525460
ChefOmarDee <ChefOmarDee@gmail.com>
2024-08-05 22:27:40 -0400

    removal of files

--- ekko-pitch-ai/app/Clean/Mane/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Mane() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			This Is Clean Mane
-		</main>
-	);
-}
--- ekko-pitch-ai/app/Clean/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Clean() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			This Is Clean
-		</main>
-	);
-}
