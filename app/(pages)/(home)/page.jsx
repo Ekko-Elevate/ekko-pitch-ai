95727db8c5db48a2b5035b9a7b6f47e31d6adcdd
AaronQTran <dahpaladin123@gmail.com>
2024-08-12 11:38:21 -0400

    navbar done

---
+++ app/(pages)/(home)/page.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Home() {
+	return (
+		<div className="flex items- justify-center min-h-screen bg-[#fff7db]">
+        	<h1 className="text-4xl font-bold text-blue-900">Welcome to the Home Page</h1>
+      	</div>
+	);
+}
