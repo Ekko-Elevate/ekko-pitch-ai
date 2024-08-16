34ce65e4c26620b9de527e15b1984d4c0ad1aa9e
ChefOmarDee <chefomardee@gmail.com>
2024-08-16 14:30:44 -0400

    changed casing

---
+++ app/api/clean/route.js
@@ -0,0 +1,8 @@
+
+import { NextResponse } from "next/server.js";
+import resizeImage from "../../_lib/imgresizer/imgresizer.js";
+
+export async function GET(req, res) {
+	resizeImage('./app/_lib/imgresizer/bat.png', './app/_lib/imgresizer/meow.jpg');
+	return NextResponse.json("success");
+}
