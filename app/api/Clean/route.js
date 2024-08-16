b6d46279c32bc80e7d52a266b4dfa1794f0fd35f
AaronQTran <dahpaladin123@gmail.com>
2024-08-16 13:53:33 -0400

    mongo changes and routing

--- app/api/Clean/route.js
+++ app/api/Clean/route.js
@@ -1,25 +1,8 @@
-import {connectToDatabase} from "../../_lib/mongoDB/db.js";
-import {User} from "../../_lib/mongoDB/user.js";
+
 import { NextResponse } from "next/server.js";
+import resizeImage from "../../_lib/imgresizer/imgresizer.js";
 
 export async function GET(req, res) {
-	await connectToDatabase();
-
-	const newUser = new User({
-	  UID: 'edp445',
-	  useremail: 'john.doe@example.com',
-	  password: 'securepassword',
-	  subscription: 'enterprise',
-	  token_amount: 3,
-	  CID: [1,2,3]
-	});
-  
-	try {
-	  const savedUser = await newUser.save();
-	  console.log('User added:', savedUser);
-	  return NextResponse.json("success");
-	} catch (error) {
-	  console.error('Error adding user:', error);
-	}
+	resizeImage('./app/_lib/imgresizer/bat.png', './app/_lib/imgresizer/meow.jpg');
+	return NextResponse.json("success");
 }
-
