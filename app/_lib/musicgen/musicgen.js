539e18da2ce56b6c2d88a552793e6f6132745050
ChefOmarDee <chefomardee@gmail.com>
2024-08-17 18:10:10 -0400

    awaited music gen

--- app/_lib/musicgen/musicgen.js
+++ app/_lib/musicgen/musicgen.js
@@ -1,6 +1,6 @@
 const Replicate = require("replicate");
 const replicate = new Replicate();
-const https = require("https"); // or 'https' for https:// URLs
+const https = require("https");
 const fs = require("fs");
 
 const input = {
@@ -11,20 +11,35 @@ const input = {
 	duration: 15,
 };
 
-export async function musicGenerator(id="1234") {
+export async function musicGenerator(id = "1234") {
 	const output = await replicate.run(
 		"meta/musicgen:671ac645ce5e552cc63a54a2bbff63fcf798043055d2dac5fc9e36a837eedcfb",
 		{ input }
 	);
 	console.log(output);
-	const file = fs.createWriteStream(`app/api/makegeneration/_music/music${id}.mp3`);
-	const request = https.get(output, function (response) {
-		response.pipe(file);
 
-		// after download completed close filestream
-		file.on("finish", () => {
-			file.close();
-			console.log("Download Completed");
-		});
+	await downloadFile(output, `app/api/makegeneration/_music/music${id}.mp3`);
+	console.log("Download Completed");
+}
+
+function downloadFile(url, outputPath) {
+	return new Promise((resolve, reject) => {
+		const file = fs.createWriteStream(outputPath);
+		https
+			.get(url, (response) => {
+				response.pipe(file);
+
+				file.on("finish", () => {
+					file.close();
+					resolve();
+				});
+
+				file.on("error", (err) => {
+					fs.unlink(outputPath, () => reject(err));
+				});
+			})
+			.on("error", (err) => {
+				fs.unlink(outputPath, () => reject(err));
+			});
 	});
 }
--- app/api/MakeGeneration/addaudio/route.js
+++ app/api/MakeGeneration/addaudio/route.js
@@ -7,9 +7,17 @@ export async function GET(request) {
 	let id = "1234";
 	await musicGenerator();
 	await voiceGenerator();
-	await mergeAudio(`./app/api/makegeneration/_voice/voice${id}.mp3`, `./app/api/makegeneration/_music/music${id}.mp3`, `./app/api/makegeneration/_audio/audio${id}.mp3`);
+	await mergeAudio(
+		`./app/api/makegeneration/_voice/voice${id}.mp3`,
+		`./app/api/makegeneration/_music/music${id}.mp3`,
+		`./app/api/makegeneration/_audio/audio${id}.mp3`
+	);
 	console.log("Audio merged");
-	await vidAddAudio(`./app/api/makegeneration/_video/video${id}.mp4`, `./app/api/makegeneration/_audio/audio${id}.mp3`, `./app/api/makegeneration/_output/output${id}.mp4`);
+	await vidAddAudio(
+		`./app/api/makegeneration/_video/video${id}.mp4`,
+		`./app/api/makegeneration/_audio/audio${id}.mp3`,
+		`./app/api/makegeneration/_output/output${id}.mp4`
+	);
 	console.log("Vid Created");
 
 	return new Response("Hello, Next.js!");
