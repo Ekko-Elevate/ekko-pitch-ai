f027106d4ba42b1069f57a872f992c14d9ef8ba6
AaronQTran <dahpaladin123@gmail.com>
2024-08-17 12:23:56 -0400

    navbar fixed and download

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -1,11 +1,11 @@
 export default function Home() {
 	return (
-		<div className="flex items- justify-center min-h-screen bg-[#fff7db]">
+		<div className="flex overflow-hidden justify-center min-h-screen bg-[#fff7db]">
         	<h1 className="text-4xl font-bold text-blue-900">Welcome to the Home Page</h1>
 			<h1 className="text-4xl font-bold text-blue-900">Welcome to the Home Page</h1>
 			<h1 className="text-4xl font-bold text-blue-900">Welcome to the Home Page</h1>
 			<h1 className="text-4xl font-bold text-blue-900">Welcome to the Home Page</h1>
-			<h1 className="text-4xl font-bold text-blue-900">Welcome to the Home Page</h1>
+			
     	</div>
 	);
 }
