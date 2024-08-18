d9809e8fd35e9a0a8159c423645b7f3d46283d70
ChefOmarDee <chefomardee@gmail.com>
2024-08-18 17:37:25 -0400

    added login callback capablities

--- app/(pages)/(dashboard)/clean/page.jsx
+++ app/(pages)/(dashboard)/clean/page.jsx
@@ -1,3 +1,41 @@
+"use client";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { useEffect } from "react";
+
 export default function Clean() {
-	return <p>clean</p>;
+	const { user, error, isLoading } = useUser();
+
+	useEffect(() => {
+		if (user) {
+			console.log(user);
+		}
+	}, [user]);
+
+	if (isLoading) return <div>Loading...</div>;
+	if (error) return <div>{error.message}</div>;
+
+	return (
+		<>
+			{user ? (
+				<div>
+					<div>
+						<img
+							src={user.picture}
+							alt={user.name}
+							style={{ borderRadius: "50%" }}
+						/>
+						<h2>{user.name}</h2>
+						<p>{user.email}</p>
+					</div>
+					<a href="/api/auth/logout">Logout</a>
+				</div>
+			) : (
+				<div>
+					<p>You are not logged in.</p>
+					<a href="/api/auth/login">Login</a>
+					<a href="/api/clean">Experimental Login</a>
+				</div>
+			)}
+		</>
+	);
 }
--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -1,3 +1,15 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin, handleCallback } from "@auth0/nextjs-auth0";
+//session is basically information about user being logged in here is documentation:https://auth0.github.io/nextjs-auth0/types/handlers_callback.AfterCallbackAppRoute.html
+//session must be the middle item, we use the "_" to ignore the 1st and 3rd parameter.
+const afterCallback = (_, session, __) => {
+	console.log(session.user);
+	console.log("michael");
+	return session;
+};
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+	login: handleLogin({
+		returnTo: "/dashboard",
+	}),
+	callback: handleCallback({ afterCallback }),
+});
