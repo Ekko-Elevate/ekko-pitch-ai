eb02534031318fb00ca6f145bfc2e37e749f6615
ChefOmarDee <chefomardee@gmail.com>
2024-08-21 10:10:10 -0400

    changed caps

--- app/(pages)/(home)/about/page.jsx
+++ app/(pages)/(home)/about/page.jsx
@@ -1,7 +1,9 @@
 export default function About() {
 	return (
 		<div className="flex items-center justify-center min-h-screen bg-blue-100">
-        	<h1 className="text-4xl font-bold text-blue-900">Welcome to the ABOUT Page</h1>
-      	</div>
+			<h1 className="text-4xl font-bold text-blue-900">
+				Welcome to the ABOUT Page
+			</h1>
+		</div>
 	);
 }
