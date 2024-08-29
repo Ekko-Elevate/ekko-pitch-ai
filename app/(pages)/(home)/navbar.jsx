be0fdb251ddf292cad1af004a0659d7ce8ba5929
AaronQTran <dahpaladin123@gmail.com>
2024-08-28 23:16:43 -0400

    navbar is fixed fr this time

--- app/(pages)/(home)/navbar.jsx
+++ app/(pages)/(home)/navbar.jsx
@@ -49,81 +49,19 @@ const Navbar = ({children}) => {
 	};
 
 	return (
-		<nav
-			className={`sticky top-0 w-full ${navbarHeight} bg-[#02254D] z-50 transition-all duration-300`}
-		>
-			<div className="w-full h-full flex justify-between items-center px-4 sm:px-6 md:px-6">
-				<div className="justify-between flex space-x-8 items-center">
-					<Link href="/">
-						<Image
-							src={Logo}
-							alt="Logo"
-							width={65}
-							height={50}
-							className="cursor-pointer"
-							priority
-						/>
-					</Link>
-					{!isMobile && (
-						<div className="justify-between flex space-x-4 items-center">
-							<Link href="/sign-up">
-								<div className="font-bold text-lg bg-[#fff7db] text-black px-4 py-2 rounded-full hover:bg-gray-400 cursor-pointer">
-									Sign in
-								</div>
-							</Link>
-							<Link href="/sign-up">
-								<div className="font-bold text-lg">Sign Up </div>
-							</Link>
-						</div>
-					)}
-					{isMobile && (
-						<div className="cursor-pointer">
-							<FaBars onClick={toggleMenu} size={24} />
-						</div>
-					)}
-					{isMobile && (
-						<>
-							{isOpen && ( //panel closer
-								<div
-									className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-40 transition-opacity duration-300"
-									onClick={toggleMenu}
-								></div>
-							)}
-							<div
-								className={`fixed top-0 right-0 w-3/4 h-full bg-[#02254D] shadow-xl z-50 flex flex-col p-4 transform transition-all duration-300 ease-in-out ${
-									//panel opener
-									isOpen ? "translate-x-0" : "translate-x-full" //conditional, isOpen = true -> translates element 0, false -> translates it to right
-								}`}
-							>
-								<div className="flex justify-end">
-									<FaTimes
-										onClick={toggleMenu}
-										size={24}
-										className="cursor-pointer"
-									/>
-								</div>
-								<Link href="/about" onClick={closeMenu}>
-									<div className="font-bold text-lg py-2">About</div>
-								</Link>
-								<Link href="/pricing" onClick={closeMenu}>
-									<div className="font-bold text-lg py-2">Pricing</div>
-								</Link>
-								<Link href="/contact" onClick={closeMenu}>
-									<div className="font-bold text-lg py-2">Contact Us</div>
-								</Link>
-								<Link href="/howtouse" onClick={closeMenu}>
-									<div className="font-bold text-lg py-2">How to Use</div>
-								</Link>
-								<Link href="/dashboard" onClick={closeMenu}>
-									<div className="font-bold text-lg py-2">Sign Up</div>
-								</Link>
-								<Link href="/dashboard" onClick={closeMenu}>
-									<div className="font-bold text-lg py-2">Sign In</div>
-								</Link>
-							</div>
-						</>
-					)}
-				</div>
+	  <nav className={`sticky top-0 w-full ${navbarHeight} bg-[#02254D] z-50 transition-all duration-300`}>
+		<div className="w-full h-full flex justify-between items-center px-4 sm:px-6 md:px-6">
+			<div className="justify-between flex space-x-8 items-center">
+				<Link href='/'>
+					<Image
+						src={Logo}
+						alt="Logo"
+						width={65}
+						height={50}
+						className="cursor-pointer"
+						priority
+					/>
+				</Link>
 				{!isMobile && (
 					<>
 					<Link href='/about'>
