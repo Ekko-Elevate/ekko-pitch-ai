8dcb7c73e583571729f3b18d449667d2d7164f68
ChefOmarDee <chefomardee@gmail.com>
2024-09-02 21:02:18 -0400

    ffmpeg-cross platform

--- app/_lib/audiomanagement/mergeaudio.js
+++ app/_lib/audiomanagement/mergeaudio.js
@@ -1,12 +1,31 @@
 const path = require("path");
 const ffmpeg = require("fluent-ffmpeg");
-const ffmpegPath = path.resolve(
-	process.cwd(),
-	"node_modules",
-	"@ffmpeg-installer",
-	"win32-x64",
-	"ffmpeg.exe"
-);
+const os = require("os");
+
+const platform = os.platform();
+const arch = os.arch();
+
+let ffmpegPath;
+
+if (platform === "win32") {
+	ffmpegPath = path.resolve(
+		process.cwd(),
+		"node_modules",
+		"@ffmpeg-installer",
+		"win32-" + arch,
+		"ffmpeg.exe"
+	);
+} else if (platform === "linux") {
+	ffmpegPath = path.resolve(
+		process.cwd(),
+		"node_modules",
+		"@ffmpeg-installer",
+		"linux-" + arch,
+		"ffmpeg"
+	);
+} else {
+	throw new Error("Unsupported platform: " + platform);
+}
 ffmpeg.setFfmpegPath(ffmpegPath);
 
 export async function mergeAudio(
